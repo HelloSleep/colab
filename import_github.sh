d116bc4a4e107ab463ca29608d4debaff00f9379
HelloSleep <nargene@gmail.com>
2023-02-19 01:20:00 +0900

    Update import_github.sh

--- import_github.sh
+++ import_github.sh
@@ -1,2 +1,10 @@
+# google drive mount
 from google.colab import drive
 drive.mount('/content/drive')
+
+# create folder in your google drive (ex) testProjcet)
+cd /content/drive/MyDrive/<drive projcet name>
+# cd /content/drive/MyDrive/testProjcet
+
+# clone target project
+!git clone <github project link>
